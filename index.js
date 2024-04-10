a6d5e4d6672060b027ea068bf3d79697d0da9c27
TusharSachdeva12 <sachdevatushar025@gmail.com>
2024-04-11 01:15:44 +0530

    Mood changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http  = require("http");
 http.createServer((req,res)=>{
     res.write("How are you ??");
-    res.write("I am good.");
+    res.write("I am happy.");
     res.end();
 }).listen(3000);
