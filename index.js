a542e227cd1d0086f176677136d6c86b16539b8e
Kshitij <kshitijk2511@gmail.com>
2024-04-22 17:18:55 +0530

    commit by kshitij

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+console.log('hello');
 const http  = require("http");
 http.createServer((req,res)=>{
     res.write("How are you ??");
+    res.write("I'm kshitij and I am good");
     res.end();
 }).listen(3000);
